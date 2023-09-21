933aeb88fe6910bde92ba40c0476695b51fac557
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-09-21 14:54:08 +0800

    Finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	const lettersArr = str.match(/\w/g);
+	const lettersSubstr = lettersArr.join('');
+	const reversedOnlyLetters = lettersArr.reverse().join('');
+	return lettersSubstr.toLowerCase() === reversedOnlyLetters.toLowerCase();
 };
 
 // Do not edit below this line
