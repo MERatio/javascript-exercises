c08a54bc58e0373d4b2de8033a6b0d5833b462f6
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-09-21 15:43:44 +0800

    Finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+	if (member < 0) {
+		return 'OOPS';
+	}
 
+	if ([0, 1].includes(member)) {
+		return member;
+	}
+
+	let prev = 1;
+	let cur = 1;
+	let curDummy = cur;
+
+	for (let i = 3; i <= member; i++) {
+		curDummy = cur;
+		cur = prev + cur;
+		prev = curDummy;
+	}
+
+	return cur;
 };
 
 // Do not edit below this line
