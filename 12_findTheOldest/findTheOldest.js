289e8388580931d6097d8864d11856a69d6e0985
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-09-21 16:07:56 +0800

    Finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+	const yearOfDeath = ('yearOfDeath' in person) 
+		? person.yearOfDeath 
+		: new Date().getFullYear();
+	const age = yearOfDeath - person.yearOfBirth;
+	return age;
+}
 
+
+const sortByAge = function(people) {
+	const oldestToYoungest = people.slice().sort((a, b) => {
+		const aAge = getAge(a);
+		const bAge = getAge(b);
+		return bAge - aAge;
+	});
+	return oldestToYoungest;
+}
+
+
+const findTheOldest = function(people) {
+	const oldestToYoungest = sortByAge(people);
+	return oldestToYoungest[0];
 };
 
 // Do not edit below this line
