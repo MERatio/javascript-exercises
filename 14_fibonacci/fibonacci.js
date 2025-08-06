22cf8404f9cd54115b9bd4f79ee34b8346bdefcd
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-08-06 11:17:14 +0800

    Complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(f) {
+ if (f < 0) {
+  return 'OOPS';
+} else if (f == 0) {
+  return 0;
+}
 
+let prev = 0;
+let cur = 1;
+
+for (let i = 1; i < f; i++) {
+  const temp = cur;
+  cur += prev;;
+  prev = temp; 
+}
+
+return cur;
 };
 
 // Do not edit below this line
