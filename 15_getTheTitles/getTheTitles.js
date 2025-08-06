6ec410bc08db57df8c5a4af6add46a1b9f1a8a19
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-08-06 11:26:45 +0800

    Complete 15_getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  return books.reduce((bookTitles, book) => {
+    return [...bookTitles, book.title];
+  }, []);
 };
 
 // Do not edit below this line
