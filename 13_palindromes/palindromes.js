1d98240e7a1bff2708a72caa745804bf496b7ee6
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-08-06 10:41:23 +0800

    Complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const wordChars = str.toLowerCase()
+    .replace(/\W/g, '')
+    .split('');  
+  const reversedWordChars = wordChars.toReversed();
+  return wordChars.join('') === reversedWordChars.join('')
 };
 
 // Do not edit below this line
