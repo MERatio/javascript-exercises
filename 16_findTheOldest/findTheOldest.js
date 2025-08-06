2180a02c1299b887efe492a846e700d6c8c573e3
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-08-06 11:43:30 +0800

    Complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  function getAge(person) {
+    let yearOfDeath = person.yearOfDeath
+    if (yearOfDeath === undefined) {
+      yearOfDeath = new Date().getFullYear();
+    }
+    return yearOfDeath - person.yearOfBirth;
+  }
 
+
+  return people.reduce((oldest, person) => {
+    if (getAge(oldest) < getAge(person)) {
+      return person;
+    } else {
+      return oldest;
+    }
+  });
 };
 
 // Do not edit below this line
